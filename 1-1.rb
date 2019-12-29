55ac5913a3acef90a6bcefbeabac0bf72cdd62d0
miyazaki <kattyan.08ms@gmail.com>
2019-12-29 20:56:42 +0900

    1-1 answer

---
+++ 1-1.rb
@@ -0,0 +1,17 @@
+# p 'A'.ord => 65
+# p 'a'.ord => 97
+
+# あえて本に載っているようにJavaの解法っぽく書いてみたけど、Rubyの配列はJavaと違って初期化した配列を越えるindexはnilになっちゃうから、ちょっとイケてないかも。とはいえ、最初に256文字以上の文字列は排除しているので、これでもいいかもしれない！！
+
+def isUniqueChars2(str)
+  return false if str.length > 256
+
+  char_set = [256]
+  str.chars.each do |char|
+    return false if char_set[char.ord]
+
+    char_set[char.ord] = true
+  end
+end
+
+p isUniqueChars2('abcdwehlvs')
