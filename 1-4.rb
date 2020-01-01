66f1edd8834f5a7cd5c9b42d9094ea78208bc980
miyazaki <kattyan.08ms@gmail.com>
2020-01-01 19:25:08 +0900

    1-4

--- 1-4.rb
+++ 1-4.rb
@@ -1,8 +1,12 @@
 # Ruby便利なメソッド多すぎる
+
 # ASCII前提なので全角スペースは考慮しない
 # stripの対象かどうかは「ASCIIでの文字コード(10進数)が32または9以上13以下」
 # 全角スペースを取り除くgemもある！！
 # https://github.com/sanfrecce-osaka/hurricane_trimar/blob/master/lib/hurricane_trimar.rb
+
+# バッファを気にしなくていい=変数を入れる箱の大きさめっちゃ大きいで
+#
 
 def rename_half_width_space(str)
   p str.strip.gsub(' ', '%20')
